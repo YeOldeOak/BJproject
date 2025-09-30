efacab4ddeb4415600395f4f971f92a4704b29db
Todd Bellavia <toddibellavia@yahoo.com>
2025-09-30 13:08:37 +0200

    Added shuffle and large deck creation

--- deck.py
+++ deck.py
@@ -1,10 +1,13 @@
 class Card:
     def __init__(self, value, suit):
         self.value = value
-        self.color = suit
+        self.suit = suit
+    
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
 
 suits = ['Diamonds','Clubs','Hearts','Spades']
 values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
 
 
-deck = [Card(value, suit) for value in values for suit in suits]
+card_deck = [Card(value, suit) for value in values for suit in suits]
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-tx = "test"
+import deck
+import random
+
+# Shuffles the deck inputted to function
+def shuffle_up(decks):
+    random.shuffle(decks)
+
+    return decks
+
+# Stacks amount_of_decks amount of 52 card decks into one big deck
+def make_decks(amount_of_decks):
+    single_deck = deck.card_deck
+    print(amount_of_decks)
+    big_deck = single_deck * amount_of_decks
+           
+    return big_deck
+
+
+
